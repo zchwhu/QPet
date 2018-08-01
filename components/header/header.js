e435fa60b71882010ef502b98bcab6fbac55ba66
itachizhao <itachizhao@tencent.com>
2018-08-02 01:58:03 +0800

    增加header组件,完成设置页面样式

---
+++ components/header/header.js
@@ -0,0 +1,12 @@
+Component({
+  options: {
+    multipleSlots: true // 在组件定义时的选项中启用多slot支持
+  },
+  properties: {
+    // 这里定义了innerText属性，属性值可以在组件使用时指定
+    title: {
+      type: String,
+      value: 'default value',
+    }
+  }
+})
